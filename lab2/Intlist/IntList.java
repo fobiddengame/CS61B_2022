b256099d578336d4805c431db068bbe45b725167
fobiddengame <panxiezhao@gmail.com>
2022-04-27 01:20:14 +0800

    second submit IntList.java

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,15 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+        IntList res = A;
+        while (A.rest != null) {
+            A = A.rest;
+        }
+        A.rest = B;
+        return res;
     }
 
     /**
@@ -91,7 +99,19 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            return B;
+        }
+        IntList res = new IntList(A.first, null);
+        IntList str = res;
+        A = A.rest;
+        while (A != null) {
+            str.rest = new IntList(A.first, null);
+            A = A.rest;
+            str = str.rest;
+        }
+        str.rest = B;
+        return res;
     }
 
 
